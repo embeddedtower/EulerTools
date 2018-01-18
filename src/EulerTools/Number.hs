a82b173e9bcf43c675fa12c6663a04d1a1b022cd
Carl Hoffman <embeddedtower@gmail.com>
2018-01-18 13:04:58 -0700

    Add expBySq: fast modular exponentiation by repeated squaring

--- src/EulerTools/Number.hs
+++ src/EulerTools/Number.hs
@@ -2,10 +2,15 @@ module EulerTools.Number
 ( countDivisors
 , totient
 , totientFromDivs
+, expBySq
 ) where
 
 import Data.List           (group)
 import Data.Numbers.Primes (primeFactors)
+
+import EulerTools.Digit
+
+-- Prime factorization functions
 
 countDivisors :: Integral a => a -> Int
 countDivisors =
@@ -19,3 +24,14 @@ totient n = n * product (map (-1+) divs) `div` product divs
 totientFromDivs :: Integral a => a -> [a] -> a
 totientFromDivs n divs =
   n * product (map (-1+) divs) `div` product divs
+
+-- Fast modular exponentiation by squaring
+
+expBySq :: Integral a => a -> a -> a -> a
+expBySq modulus base =
+  foldr (\a b -> mod (a*b) modulus) 1 . ebsWorker base . getBaseDigits 2
+  where
+    square n = mod (n*n) modulus
+    ebsWorker _ []     = []
+    ebsWorker n (b:bs) | b == 1    = n : ebsWorker (square n) bs
+                       | otherwise = 1 : ebsWorker (square n) bs
