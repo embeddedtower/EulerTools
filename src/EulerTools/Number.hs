a58152d5d80051763ece3a2001424d0c095bbbeb
Carl Hoffman <carlh@MacBook-Pro-OF-DOOM.local>
2018-01-18 01:50:20 -0700

    Add totient function

--- src/EulerTools/Number.hs
+++ src/EulerTools/Number.hs
@@ -1,5 +1,6 @@
 module EulerTools.Number
 ( countDivisors
+, totient
 ) where
 
 import Data.List           (group)
@@ -8,3 +9,8 @@ import Data.Numbers.Primes (primeFactors)
 countDivisors :: Integral a => a -> Int
 countDivisors =
   product . map ((+1) . length) . group . primeFactors
+
+totient :: Integral a => a -> a
+totient n = n * product (map (-1+) divs) `div` product divs
+  where
+    divs = map head . group $ primeFactors n
