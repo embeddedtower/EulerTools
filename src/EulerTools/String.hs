aee7d872308391d17bc866504479b183128c3e04
Carl Hoffman <carlh@MacBook-Pro-OF-DOOM.local>
2018-01-17 17:57:57 -0700

    Add mkPalindromes: produce palindromes with b symbols and length n

--- src/EulerTools/String.hs
+++ src/EulerTools/String.hs
@@ -1,6 +1,13 @@
 module EulerTools.String
 ( isPalindrome
+, mkPalindromes
 ) where
 
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome as = as == reverse as
+
+mkPalindromes :: Int -> Int -> [[Int]]
+mkPalindromes b n
+  | n == 0    = [[]]
+  | n == 1    = [ [k] | k <- [0..b-1] ]
+  | otherwise = [ k:p ++ [k] | k <- [0..b-1] , p <- mkPalindromes b (n-2) ]
