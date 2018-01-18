e2cc0078f099c601535a33bac599c6fe6d316d60
Carl Hoffman <embeddedtower@gmail.com>
2018-01-18 01:57:17 -0700

    Add alternate totient function with explicit divisor list

--- src/EulerTools/Number.hs
+++ src/EulerTools/Number.hs
@@ -14,3 +14,7 @@ totient :: Integral a => a -> a
 totient n = n * product (map (-1+) divs) `div` product divs
   where
     divs = map head . group $ primeFactors n
+
+totientFromDivs :: Integral a => a -> [a] -> a
+totientFromDivs n divs =
+  n * product (map (-1+) divs) `div` product divs
