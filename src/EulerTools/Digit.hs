86853e4126318368ad42fec1913a715705f05f7d
Carl Hoffman <carlh@MacBook-Pro-OF-DOOM.local>
2018-01-17 18:29:48 -0700

    Add fromBaseDigits: construct an integer from its base-b digits

--- src/EulerTools/Digit.hs
+++ src/EulerTools/Digit.hs
@@ -6,3 +6,7 @@ getBaseDigits :: Integral a => a -> a -> [a]
 getBaseDigits b n
   | n < b    = [n]
   | otherwise = mod n b : getBaseDigits b (div n b)
+
+fromBaseDigits :: Integral a => a -> [a] -> a
+fromBaseDigits b []     = 0
+fromBaseDigits b (d:ds) = d + b * fromBaseDigits b ds
