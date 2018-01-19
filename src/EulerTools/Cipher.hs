80e72cafe4e7ae458e0a3f736efba7018de762f7
Carl Hoffman <embeddedtower@gmail.com>
2018-01-19 00:11:42 -0700

    Actually add Cipher.hs to repository

---
+++ src/EulerTools/Cipher.hs
@@ -0,0 +1,23 @@
+module EulerTools.Cipher
+( getAlphaValue
+, toAlphaLower
+, toAlphaUpper
+) where
+
+import Data.Char (ord, chr, isUpper, isLetter)
+
+getAlphaValue :: Char -> Int
+getAlphaValue c
+  | not $ isLetter c = error "can't convert non-letter"
+  | isUpper c = ord c - 64
+  | otherwise = ord c - 96
+
+toAlphaLower :: Int -> Char
+toAlphaLower n
+  | n < 1 || n > 26 = error "value out of range"
+  | otherwise = chr $ n + 96
+
+toAlphaUpper :: Int -> Char
+toAlphaUpper n
+  | n < 1 || n > 26 = error "value out of range"
+  | otherwise = chr $ n + 64
